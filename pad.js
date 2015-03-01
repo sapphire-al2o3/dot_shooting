84cf234d9a351bf04e7c8e105a54a04ee185019d
tel <t_telkun@hotmail.com>
2015-03-01 23:12:12 +0900

    パッド処理の修正

--- pad.js
+++ pad.js
@@ -1,16 +1,28 @@
 var Pad = function() {
 	'use strict';
 	
-	var keyState = [];
+	var keyState = [],
+		prevKeyState = [],
+		chenged = [];
 	
 	return {
 		getKey: function(k) {
 			return keyState[k];
 		},
+		getKeyDown: function(k) {
+			return changed[k] & keyState[k];
+		},
+		getKeyUp: function(k) {
+			return changed[k] & !keyState[k];
+		},
 		update: function() {
 			var pad = navigator.webkitGetGamepads()[0];
 			if (pad) {
-				pad.buttons[i];
+				for(var i = 0; i < pad.buttons.length; i++) {
+					keyState[i] = pad.buttons[i];
+					changed[i] = keyState[i] ^ prevKeyState[i];
+					prevKeyState[i] = keyState[i];
+				}
 			}
 		}
 	};
