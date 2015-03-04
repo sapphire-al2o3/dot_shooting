988a8c1054b91ca6fd7a6822e52934500ff3f4c0
sapphire-al2o3 <t_telkun@hotmail.com>
2015-03-04 22:09:45 +0900

    パーティクル処理を作る

--- particle.js
+++ particle.js
@@ -3,5 +3,13 @@ var Particle = function() {
 };
 
 var Emitter = function() {
+	this.particles = [];
+};
+
+Emitter.prototype.emit = function() {
 	
 };
+
+Emitter.prototype.update = function() {
+	
+}
